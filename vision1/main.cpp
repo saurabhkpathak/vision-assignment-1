00a4817a8e6ef690f2b67f15d5db4734c3df6d76
Saurabh Pathak <pathaks@tcd.ie>
2016-10-23 20:30:47 +0100

    adding red pixel identification code

--- vision1/main.cpp
+++ vision1/main.cpp
@@ -13,12 +13,40 @@
 
 using namespace cv;
 
-int main(int argc, const char * argv[]) {
-    VideoCapture cap(0);
-    while (true) {
-        Mat Webcam;
-        cap.read(Webcam);
-        imshow("Webcam", Webcam);
+int main(int argc, const char** argv) {
+    
+    Mat image = imread("img.jpg", IMREAD_COLOR);
+    
+    if( image.empty())
+    {
+        std::cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
     }
+    
+    //    int current_threshold = 0, max_threshold = 255;
+    
+    Mat hsv_image, lower_red_hue_range, upper_red_hue_range;
+    
+    cvtColor(image, hsv_image, COLOR_BGR2HSV);
+    
+    inRange(hsv_image, Scalar(0, 100, 100), Scalar(10, 255, 255), lower_red_hue_range);
+    inRange(hsv_image, Scalar(160, 100, 100), Scalar(179, 255, 255), upper_red_hue_range);
+    
+    Mat red_hue_image;
+    addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
+    
+    //    GaussianBlur(red_hue_image, red_hue_image, Size(9, 9), 2, 2);
+    
+    //    threshold(hsv_img,otsu_binary_image,current_threshold,max_threshold,
+    //              THRESH_BINARY | THRESH_OTSU);
+    
+    //    Mat otsu_binary_image_display;
+    
+    //    cvtColor(otsu_binary_image, otsu_binary_image_display, CV_GRAY2BGR);
+    
+    namedWindow( "Display window", WINDOW_AUTOSIZE );
+    imshow( "Display window", red_hue_image );
+    
+    waitKey(0);
     return 0;
 }
