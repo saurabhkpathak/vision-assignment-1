0ef1e6e7ab167da0e0ccf3d27b66f5bd3d61fe4f
Saurabh Pathak <pathaks@tcd.ie>
2016-10-23 21:06:19 +0100

    adding comments

--- vision1/main.cpp
+++ vision1/main.cpp
@@ -14,36 +14,36 @@
 using namespace cv;
 
 int main(int argc, const char** argv) {
-    
-    Mat image = imread("img.jpg", IMREAD_COLOR);
-    
+
+    // reading image in Mat object
+    Mat image = imread("img.jpg", 1);
+
+    // checking if image was read successfully
     if( image.empty())
     {
+        // if image object is empty, display error message on console
         std::cout <<  "Could not open or find the image" << std::endl ;
         return -1;
     }
-    
-    //    int current_threshold = 0, max_threshold = 255;
-    
+
+    // declaring varibales to store HSV converted image
+    // and red pixel upper, lower hue range
     Mat hsv_image, lower_red_hue_range, upper_red_hue_range;
-    
+
+    // converting BGR image to HSV
     cvtColor(image, hsv_image, COLOR_BGR2HSV);
-    
+
+    // thresholding image as per hue range of red pixels
+    // i.e. 0-10 and 160-179. keeping only red pixels
     inRange(hsv_image, Scalar(0, 100, 100), Scalar(10, 255, 255), lower_red_hue_range);
     inRange(hsv_image, Scalar(160, 100, 100), Scalar(179, 255, 255), upper_red_hue_range);
-    
+
     Mat red_hue_image;
+    
+    // adding weighted images obtained from hue thresholded images
     addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
-    
-    //    GaussianBlur(red_hue_image, red_hue_image, Size(9, 9), 2, 2);
-    
-    //    threshold(hsv_img,otsu_binary_image,current_threshold,max_threshold,
-    //              THRESH_BINARY | THRESH_OTSU);
-    
-    //    Mat otsu_binary_image_display;
-    
-    //    cvtColor(otsu_binary_image, otsu_binary_image_display, CV_GRAY2BGR);
-    
+
+
     namedWindow( "Display window", WINDOW_AUTOSIZE );
     imshow( "Display window", red_hue_image );
     
